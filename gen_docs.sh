2334fc44fd205df1d0b3bf35fa038d9602a3a075
Andreas Unterkircher <unki@netshadow.at>
2008-12-23 09:54:47 +0100

    * enhance v1.5 UPGRADE text a bit * ignore themes-directory when generating the phpDocumentator stuff, gen_docs.sh * update documentation to 1.5
    Signed-off-by: Andreas Unterkircher <unki@netshadow.at>

--- gen_docs.sh
+++ gen_docs.sh
@@ -5,7 +5,7 @@
 phpdoc -o HTML:frames:earthli \
        -d . \
        -t docs \
-       -i 'debian/,thumbs/,resources/,templates_c/,nephthys_cfg.php' \
+       -i 'debian/,thumbs/,resources/,templates_c/,themes/,nephthys_cfg.php' \
        -ti 'Nephthys source-code documentation' \
        -dn 'nephthys' \
        -s \
